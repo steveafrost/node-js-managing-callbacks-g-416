6d72e1e398dd23454bf2dccdfcb2456e6d8d83f1
Ethan Zane Miller <ethanzanemiller@gmail.com>
2016-04-25 12:32:36 +0300

    Add stub for code-along

---
+++ async-example.js
@@ -0,0 +1,7 @@
+const prompt  = require('prompt');
+const request = require('request');
+const async   = require('async');
+
+const WEATHER_API_URL = 'http://api.openweathermap.org/data/2.5/weather?q='
+const WEATHER_API_KEY = '88bfde3533d98e8a300a138ef668cda2';
+
